fe0b4ee0e0d39691112fcba36a1e5e7093edef9a
hailtonpatrick <instrutorpatrickaraujo@gmail.com>
2025-05-23 19:26:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 function mostrarTela(id) {
-  // Oculta todas as telas que comecem com "tela"
   const telas = document.querySelectorAll('div[id^="tela"]');
   telas.forEach(tela => tela.classList.add('tela-oculta'));
 
-  // Mostra a tela desejada
   const telaEscolhida = document.getElementById(id);
   if (telaEscolhida) {
     telaEscolhida.classList.remove('tela-oculta');
   }
 }
 
-// Redimensiona o mapa interativo ao carregar
 window.addEventListener('load', function() {
   if (typeof imageMapResize === 'function') {
     imageMapResize();
