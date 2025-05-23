648f3caffa3c0a00f8115d3d98aa19e221734e41
hailtonpatrick <instrutorpatrickaraujo@gmail.com>
2025-05-23 09:04:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function mostrarTela(id) {
+  // Oculta todas as telas
+  const telas = document.querySelectorAll('div[id^="tela"]');
+  telas.forEach(tela => tela.classList.add('tela-oculta'));
+
+  // Mostra a tela escolhida
+  const telaEscolhida = document.getElementById(id);
+  telaEscolhida.classList.remove('tela-oculta');
+}
 
